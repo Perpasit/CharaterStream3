c84be58f4c74175fc63439c36442452da04d419d
Student <Student@5CD3019B0L.control.ad.sit.kmutt.ac.th>
2023-04-25 15:41:18 +0700

    Version1

---
+++ src/main/java/characterstream3/LAB4.java
@@ -0,0 +1,30 @@
+
+package characterstream3;
+
+import java.io.*;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class LAB4 {
+
+    
+    public static void main(String[] args) {
+        try {
+            FileReader file = new FileReader("data.txt");
+            Scanner input = new Scanner(file);
+            String name = input.nextLine();
+            System.out.println(name);
+            int age = input.nextInt();
+            System.out.println(age);
+            Double weight = input.nextDouble();
+            System.out.println(weight);
+            Double height = input.nextDouble();
+            System.out.println(height);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(LAB4.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+}
---
+++ src/main/java/characterstream3/LAB6.java
@@ -0,0 +1,24 @@
+
+package characterstream3;
+
+import java.util.*;
+
+
+public class LAB6 {
+
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter name: ");
+        String s = input.nextLine();
+        System.out.println(s);
+        System.out.print("Enter age: ");
+        int age = input.nextInt();
+        System.out.println(age);
+        System.out.print("Enter height: ");
+        double height = input.nextDouble();
+        System.out.println(height);
+        
+    }
+    
+}
